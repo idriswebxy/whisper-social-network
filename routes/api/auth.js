72dfd551f611db69f45039c0657c920e233f3bbf
Idris <icoder112@gmail.com>
2019-08-05 18:31:18 -0400

    server updates

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,21 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/Users");
 const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
 const config = require("config");
 
-module.exports = function(req, res, next) {
-  //  Get token from header
-  const token = req.header("x-auth-token");
-
-  // check if no token
-  if (!token) {
-    return res.status(401).json({ msg: "No token, authorization denied " });
-  }
-
+router.get("/", auth, async (req, res) => {
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
-
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(500)
+    const user = await User.findById(req.user);
+    res.json(user);
+  } catch (error) {
+    res.status(500).send("Server Error!");
   }
-};
+});
+
+router.post("/", [
+  check("email", "Please include a valid email").isEmail(),
+  check(
+    "password",
+    "Please enter a password with 6 or more characters"
+  ).isLength({ min: 6 })
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { name, email, password } = req.body;
+}
+);
+
+module.exports = router;
