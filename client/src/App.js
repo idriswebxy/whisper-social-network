6489c23f9a0572a0c3512fb3d795f89d922cede1
Idris <icoder112@gmail.com>
2019-08-13 16:35:59 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,17 @@
+import { BrowserRouter as Router } from 'react-router-dom';
 import React, { Component } from "react";
-// import Login from "./components/Login";
+import Login from "../src/components/auth/Login";
+import Navbar from "./components/Navbar/Navbar";
 
 export default class App extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
-
-  onSubmit = (e) => {
-    e.preventDefault();
-    fetch("http://localhost:5000/api/auth")
-    .then(res => res.text())
-    .then(body => console.log(body));
-  };
-
-  onChange = e => {
-    this.setState({ email: e.target.value });
-  };
-
   render() {
     return (
-    <div>
-      <form onSubmit={this.onSubmit}>
-      Name:
-        <input type="email"/>
-        Password:
-        <input type="password"/>
-        <button onClick={this.onSubmit} >Submit</button>
-      </form>
-    </div>
-    )
+      <Router>
+        <div>
+          <Navbar />
+          <Login />
+        </div>
+      </Router>
+    );
   }
 }
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import './auth.css';
+
+
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <div class="login-page">
+          <div class="form">
+            <form class="register-form">
+              <input type="text" placeholder="name" />
+              <input type="password" placeholder="password" />
+              <input type="text" placeholder="email address" />
+              <button>create</button>
+              <p class="message">
+                Already registered? <a href="#">Sign In</a>
+              </p>
+            </form>
+            <form class="login-form">
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <button>login</button>
+              <p class="message">
+                Not registered? <a href="#">Create an account</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
