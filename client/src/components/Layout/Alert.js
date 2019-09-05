255b19bea2b704f45653f264278348e82dfb34eb
Idris <icoder112@gmail.com>
2019-09-05 12:53:58 -0400

    component updates

---
+++ client/src/components/Layout/Alert.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from "react-redux"
+
+
+const Alert = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Alert.propTypes = {
+
+}
+
+const mapStateToProps = state => ({
+  alerts:   
+})
+
+export default connect()(Alert)
