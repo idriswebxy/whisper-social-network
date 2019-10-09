a311ccfdba2aeeaf93b3e8824ff7084f1cfaa22a
Idris <icoder112@gmail.com>
2019-10-09 01:19:52 -0400

    component updates

---
+++ client/src/components/Profile/CreateProfile.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from "react-redux";
+
+
+
+const CreateProfile = props => {
+
+  const [formData, setFormData] = useState({
+    
+  })
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+CreateProfile.propTypes = {
+
+}
+
+export default CreateProfile
