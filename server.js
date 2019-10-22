2dcf56a9a013954e91603bb47cadb5818109f954
Idris <icoder112@gmail.com>
2019-10-22 17:40:44 -0400

    updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const path = require("path");
-const config = require('config');
+
 
 
 const app = express();
@@ -11,7 +11,8 @@ app.use(cors());
 
 connectDB();
 
-app.use(express.json({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded( { extended: false }))
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
