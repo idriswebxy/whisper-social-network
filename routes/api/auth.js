6d781d3207b97bf8054b5501bfa5301968a5776a
Idris <icoder112@gmail.com>
2019-10-22 02:39:22 -0400

    removing node_modules

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt-nodejs");
 const auth = require("../../middleware/auth");
 const jwt = require("jsonwebtoken");
 const config = require("config");
