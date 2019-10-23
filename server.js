60821107b4b0aa23f1b15f7410cb7decd17a61c9
Idris <icoder112@gmail.com>
2019-10-23 12:27:21 -0400

    updates

--- server.js
+++ server.js
@@ -18,12 +18,13 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
   });
-
+}
 
 const PORT = process.env.PORT || 5000;
 
