7c538d007a94a581eba63edbe43124f64d2e82ba
Idris <icoder112@gmail.com>
2019-10-23 15:45:56 -0400

    updates

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
-
-
 
 const app = express();
 
@@ -11,22 +9,18 @@ const app = express();
 
 connectDB();
 
-
-app.use(express.json( { extended: false }))
-
+app.use(express.json({ extended: false }));
 
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
