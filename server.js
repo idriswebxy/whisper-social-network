62eb1105bcc7e964606c90a476c0b7a08c71cc0d
Idris <icoder112@gmail.com>
2019-10-23 12:38:07 -0400

    updates

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config()
+
 
 
 const app = express();
